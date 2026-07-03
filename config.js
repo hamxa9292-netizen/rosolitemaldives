8459ec9242bb42790a44ab8eb9471e0fb128f13d
hamxa9292-netizen <hamxa9292@gmail.com>
2026-07-04 02:07:21 +0500

    Config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const CONFIG = {
   // WhatsApp number that receives orders.
   // Country code + number, digits only. Maldives = 960.
   // Example: "9607771234"   <-- REPLACE THIS BEFORE LAUNCH
-  whatsappNumber: "960XXXXXXX",
+  whatsappNumber: "9609900378",
 
   // Currency label shown next to prices
   currency: "MVR",
